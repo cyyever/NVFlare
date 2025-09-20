734c09e752c6f30af69e24218f203d054718a4d2
Yuanyuan Chen <cyyever@outlook.com>
2025-09-20 10:15:38 +0800

    Add code
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>

--- examples/advanced/llm_hf/plot.py
+++ examples/advanced/llm_hf/plot.py
@@ -5,35 +5,35 @@ import seaborn as sns
 
 loss = {
     "Round": [0, 1, 2, 3, 4],
-    "FLOAT16": [
+    "float16": [
         2.542977809906006,
         1.785860538482666,
         1.7768937349319458,
         1.7765766382217407,
         1.7799557447433472,
     ],
-    "FLOAT4": [
+    "float4": [
         2.880049467086792,
         1.9248440265655518,
         1.9041017293930054,
         1.8973793983459473,
         1.894836664199829,
     ],
-    "NORMFLOAT4": [
+    "normalized float4": [
         2.707158088684082,
         1.8648680448532104,
         1.8497788906097412,
         1.8457902669906616,
         1.8462167978286743,
     ],
-    "ADAQUANT": [
+    "adaquant": [
         3.9254233837127686,
         2.0198545455932617,
         1.994623064994812,
         1.9854929447174072,
         1.955665946006775,
     ],
-    "BLOCKWISE8": [
+    "blockwise8": [
         2.5443713665008545,
         1.7876044511795044,
         1.7765042781829834,
@@ -42,11 +42,13 @@ loss = {
     ],
 }
 df = pandas.DataFrame(loss)
-dfl = pandas.melt(df, ["Round"], value_name="Test Loss", var_name="Scheme")
+dfl = pandas.melt(df, ["Round"], value_name="Test Loss", var_name="mode")
 
 
-sns.lineplot(data=dfl, x="Round", y="Test Loss", hue="Scheme")
+ax = sns.lineplot(data=dfl, x="Round", y="Test Loss", hue="mode")
 
 # plt.title("Test loss of different quantization schemes")
 plt.xlabel("Round")
 plt.show()
+fig = ax.get_figure()
+fig.savefig("output.png")
